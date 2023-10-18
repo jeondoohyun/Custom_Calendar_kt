a95091f0dc9d1b41089c0934d3f4415aa712c40b
dhjeon <sonlcr1@naver.com>
2023-10-19 00:02:45 +0900

    첫줄 띄고 날짜 생성되는 문제 수정할것. part3 진행중

--- app/src/main/java/com/jdh/calendar_kt/MainActivity.kt
+++ app/src/main/java/com/jdh/calendar_kt/MainActivity.kt
@@ -10,8 +10,11 @@ import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.jdh.calendar_kt.databinding.ActivityMainBinding
 import java.time.LocalDate
+import java.time.YearMonth
 import java.time.format.DateTimeFormatter
 
 
@@ -40,7 +43,7 @@ class MainActivity : AppCompatActivity() {
         setMonthView()
 
         var touchPoint = 0f
-        binding.activityCustomCalendar.setOnTouchListener(OnTouchListener { v, event ->
+        binding.recyclerView.setOnTouchListener(OnTouchListener { v, event ->
 //            val transaction: FragmentTransaction =
 //                getActivity().getSupportFragmentManager().beginTransaction()
 //            val ff: Fragment
@@ -57,10 +60,12 @@ class MainActivity : AppCompatActivity() {
                     }
                     if (touchPoint > 0) {
                         // 손가락을 우->좌로 움직였을때 오른쪽 화면 생성
+                        Toast.makeText(this, "우",Toast.LENGTH_SHORT).show()
                         selectedDate = selectedDate.plusMonths(1)
                         setMonthView()
                     } else {
                         // 손가락을 좌->우로 움직였을때 왼쪽 화면 생성
+                        Toast.makeText(this, "좌",Toast.LENGTH_SHORT).show()
                         selectedDate = selectedDate.minusMonths(1)
                         setMonthView()
                     }
@@ -109,6 +114,21 @@ class MainActivity : AppCompatActivity() {
     private fun setMonthView() {
         //년월 텍스트뷰 셋팅
         binding.monthYearText.text = monthYearFromDate(selectedDate)
+
+        //날짜 생성해서 리스트에 담기
+        val dayList = dayInMonthArray(selectedDate)
+
+        //어댑터 초기화
+        val adapter = CalendarAdapter(dayList)
+
+        //레이아웃 설정(열 7개)
+        var manager: RecyclerView.LayoutManager = GridLayoutManager(applicationContext, 7)
+
+        //레이아웃 적용
+        binding.recyclerView.layoutManager = manager
+
+        //어댑터 적용
+        binding.recyclerView.adapter = adapter
     }
 
     //날짜 타입 설정
@@ -121,5 +141,33 @@ class MainActivity : AppCompatActivity() {
         return date.format(formatter)
     }
 
+    //날짜 생성
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun dayInMonthArray(date: LocalDate): ArrayList<String>{
+
+        var dayList = ArrayList<String>()
+
+        var yearMonth = YearMonth.from(date)
+
+        //해당 월 마지막 날짜 가져오기(예: 28, 30, 31)
+        var lastDay = yearMonth.lengthOfMonth()
+
+        //해당 월의 첫 번째 날 가져오기(예: 4월 1일)
+        var firstDay = selectedDate.withDayOfMonth(1)
+
+        //첫 번째날 요일 가져오기(월:1, 일: 7)
+        var dayOfWeek = firstDay.dayOfWeek.value
+
+        // todo : 23년 10월 1일이 첫줄 띄고 생성되어 있다. 여기서 설정 해야 할듯
+        for(i in 1..41){
+            if(i <= dayOfWeek || i > (lastDay + dayOfWeek)){
+                dayList.add("")
+            }else{
+                dayList.add((i - dayOfWeek).toString())
+            }
+        }
+
+        return dayList
+    }
 
 }
